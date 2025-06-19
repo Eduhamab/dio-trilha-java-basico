36f4e63d55a34d70aed4bdd4c8713455a6eaa3fd
EduHamab <eduhamab@gmail.com>
2025-06-19 13:59:20 -0300

    Programa atualizado

--- dio-trilha-java-basico/Contabanco/src/ContaTerminal.java
+++ dio-trilha-java-basico/Contabanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite seu nome: ");
+        String name = sc.nextLine();
+
+        System.out.print("Por favor Digite o numero da sua Agencia com o digito: ");
+        String numberAgency = sc.next();
+
+        System.out.print("Por favor Digite o numero da sua conta: ");
+        int numberAccount = sc.nextInt();
+
+        System.out.print("Digite o valor que deseja depositar: ");
+        double deposit = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é: %s, conta: %d e seu saldo de: %.2f, já esta disponível para saque.  ",name, numberAgency,numberAccount,deposit);
+
+
+
+
     }
 }
